e68efb95ec4c1e72306c47917b01e5526a251b1e
buzeshouduan <365056702@qq.com>
2018-10-23 16:57:07 +0800

    123

--- gaokao.py
+++ gaokao.py
@@ -10,11 +10,13 @@ ori_data=pd.read_excel("data/高考志愿.xlsx")
 city_list=pd.read_excel("data/中国城市等级.xlsx")
 ori_data=ori_data[ori_data['2015年平均线']!='无']
 
+#将字符型转换成数值型
 kind_data=pd.get_dummies(ori_data['类型'],prefix='类型')
 yn985_data=pd.get_dummies(ori_data['是否为985'],prefix='yesno985')
 yn211_data=pd.get_dummies(ori_data['是否为211'],prefix='yesno211')
 yn_zizhu_data=pd.get_dummies(ori_data['是否自主招生'],prefix='yesno_zizhu')
 
+#所在地变量太多，分为6类
 first_tier=city_list['一线城市'][city_list['一线城市'].notnull()].tolist()
 for i in range(len(first_tier)):
     if '市' in first_tier[i]:
@@ -55,22 +57,30 @@ for i in range(len(local_list)):
 city_class_list=[]
 for i in range(len(local_list)):
     if local_list[i] in first_tier:
-        city_class_list.append("城市A")
+        city_class_list.append("A")
     elif local_list[i] in new_first_tier:
-        city_class_list.append("城市B")
+        city_class_list.append("B")
     elif local_list[i] in second_tier:
-        city_class_list.append("城市C")
+        city_class_list.append("C")
     elif local_list[i] in third_tier:
-        city_class_list.append("城市D")
+        city_class_list.append("D")
     elif local_list[i] in fourth_tier:
-        city_class_list.append("城市E")
+        city_class_list.append("E")
     elif local_list[i] in fifth_tier:
-        city_class_list.append("城市F")
+        city_class_list.append("F")
     else:
-        city_class_list.append("城市Z")
+        city_class_list.append("Z")
 
-city_class=pd.get_dummies(pd.Series(city_class_list))
-new_data=pd.concat([ori_data,city_class,kind_data,yn985_data,yn211_data,yn_zizhu_data],axis=1)
+belong_list=[]
+for i in range(len(ori_data['隶属'].tolist())):
+    if ori_data['隶属'].tolist()[i]=='教育部':
+        belong_list.append("是")
+    else:
+        belong_list.append("否")
+
+belong=pd.get_dummies(pd.Series(belong_list),prefix="yesno教育部")
+city_class=pd.get_dummies(pd.Series(city_class_list),prefix="城市")
+new_data=pd.concat([ori_data,city_class,belong,kind_data,yn985_data,yn211_data,yn_zizhu_data],axis=1)
 new_data=new_data.drop(['所在地','隶属','类型','地址','是否为985','是否为211','是否自主招生'],axis=1)
 
 new_data['2015年平均线']=new_data['2015年平均线'].astype(float)
@@ -85,8 +95,14 @@ new_data['博士点（个）'].value_counts().plot.bar(ax=axarr[1][0])
 axarr[1][0].set_title("博士数量")
 new_data['硕士点（个）'].value_counts().plot.bar(ax=axarr[1][1])
 axarr[1][1].set_title("硕士数量")
-plt.subplots_adjust(hspace=.3)
-plt.show()
+#plt.show()
+
+#print(new_data['2015年平均线'].describe())#[397,694]
+#print(new_data['院士（位）'].describe())#[0,70]
+#print(new_data['重点学科(个)'].describe())#[0,81]
+#print(new_data['博士点（个）'].describe())#[0,283]
+#print(new_data['硕士点（个）'].describe())#[0,345]
+
 
 
 
